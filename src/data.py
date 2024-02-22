b1845f577d7986ef67e7a25a9a2cbb26c75ad4b8
Mayank Soni <mayank.soni101@gmail.com>
2024-02-22 14:33:43 +0800

    Create simulator to get best guesses

--- src/data.py
+++ src/data.py
@@ -2,19 +2,24 @@ from collections.abc import Generator, Iterable
 import pathlib
 import pickle
 
+import numpy as np
 from tqdm import tqdm
 import wordfreq
 
 data_folder: pathlib.Path = pathlib.Path(__file__).parent.parent / "data"
 solutions_path: pathlib.Path = data_folder / "wordlist_nyt20230701_hidden.txt"
 guesses_path: pathlib.Path = data_folder / "wordlist_nyt20220830_all.txt"
-frequencies_path: pathlib.Path = data_folder / "word_frequencies.pkl"
+probabilities_path: pathlib.Path = data_folder / "word_probabilities.pkl"
+first_guess_indices_path: pathlib.Path = data_folder / "first_guess_wordlists.pkl"
+
+language: str = "en"
 
 
-def get_words(filepath: pathlib.Path) -> Generator[str, None, None]:
-    """Yield words from a file storing one word per line."""
-    for line in filepath.open():
-        yield line.strip()
+def get_words(filepath: pathlib.Path) -> list[str]:
+    """Return a list of words from the file at filepath.
+    One word per line."""
+    with filepath.open() as file:
+        return file.read().splitlines()
 
 
 def get_zipf_frequencies(
@@ -23,9 +28,29 @@ def get_zipf_frequencies(
     """Return a dictionary with the frequency (Zipf scale) of each word in words.
     Optionally, save the dictionary to a pickle file at save_path."""
     frequencies: dict[str, float] = {}
+    print("Getting Zipf frequencies for words")
     for word in tqdm(words):
-        frequencies[word] = wordfreq.zipf_frequency(word, "en")
+        frequencies[word] = wordfreq.zipf_frequency(word, language)
     if save_path:
         with save_path.open("wb") as file:
             pickle.dump(frequencies, file)
     return frequencies
+
+
+def get_modified_word_probabilities(
+    words: list[str], save_path: pathlib.Path | None = None
+) -> np.ndarray:
+    """Return a dictionary with probability of occurence of each word in words.
+    Probability is calculated as the normalised sigmoid of the Zipf frequency.
+    Since zipf frequencies lie between 0 and 9, this ensures that the min probability is >= 0.5 * the max probability.
+    For the wordle english list, it also leads to a distribution where ~90% of the words
+    have a probability within 0.9 of the max probability. These are desirable properties for the distribution
+    since most wordle answers, even if uncommon words, are approximately equally likely to occur.
+    Only the really rare words should have a much lower probability, but even then not much lower.
+    """
+    zipf_frequencies: np.ndarray = np.array(
+        [wordfreq.zipf_frequency(word, language) for word in words]
+    )
+    sigmoid_frequencies: np.ndarray = 1 / (1 + np.exp(-zipf_frequencies))
+    probabilities = sigmoid_frequencies / np.sum(sigmoid_frequencies)
+    return probabilities
--- src/solver.py
+++ src/solver.py
@@ -1,7 +1,10 @@
 # import argparse
-from collections import Counter
+from collections import Counter, defaultdict
 
 import numpy as np
+from tqdm import tqdm
+
+from src.wordle import check_word
 
 
 def choose_word(words: list[str]) -> str:
@@ -22,6 +25,36 @@ def choose_word(words: list[str]) -> str:
 
 def entropy(probs: np.ndarray) -> float:
     return -np.sum(probs * np.log2(probs))
+
+
+def get_best_guess(
+    solutions: list[str], allowed_guesses: list[str], solution_probabilities: np.ndarray
+) -> tuple[str, dict[tuple[int, ...], list[int]]]:
+    """Return the word from allowed_guesses that maximises the expected reduction in entropy.
+    Also return the indices of the subsets of solutions that would remain
+    based on each possible outcome of the best guess.
+    """
+    lowest_average_entropy: float = float("inf")
+    for guess in tqdm(allowed_guesses):
+        results: dict[tuple[int, ...], list[int]] = defaultdict(list)
+        # Get subsets of solutions based on the result you'd get for the guess
+        for index, solution in enumerate(solutions):
+            outcome: tuple[int, ...] = check_word(guess, solution)
+            results[outcome].append(index)
+        # Calculate the weighted average entropy of the subsets
+        # TODO: vectorise the average entropy calculation
+        # TODO: if multiple guesses have the same entropy, choose the one that could actually be a solution
+        entropies = []
+        total_probs = []
+        for _, indices in results.items():
+            entropies.append(entropy(solution_probabilities[indices]))
+            total_probs.append(np.sum(solution_probabilities[indices]))
+        average_entropy = np.average(entropies, weights=total_probs)
+        if average_entropy < lowest_average_entropy:
+            best_guess: str = guess
+            best_results: dict[tuple[int, ...], list[int]] = results
+            lowest_average_entropy = average_entropy
+    return best_guess, best_results
 
 
 # if __name__ == "__main__":
